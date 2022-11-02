e17f312528de72e897c54b9a0fd56c1ed51455d9
somnathraz <somnathkhadanga810@gmail.com>
2022-11-02 19:49:35 +0530

    email for user

--- pages/api/User/register.js
+++ pages/api/User/register.js
@@ -1,7 +1,18 @@
 import { connectToDatabase } from "../../../lib/mongodb";
 import bcrypt from "bcrypt";
+const nodemailer = require("nodemailer");
 
 const saltRounds = 12;
+const transporter = nodemailer.createTransport({
+  port: 465,
+  host: "smtp.gmail.com",
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
+  },
+  secure: true,
+});
+let emailSent = "";
 
 export default async function handler(req, res) {
   const { email, password, role, team } = req.body;
@@ -26,16 +37,38 @@ export default async function handler(req, res) {
           role: role,
           team: team,
         });
+        const mailData = {
+          from: "admissions@learnbay.co",
+          to: email,
+          subject: `Your backend username and password`,
+
+          html: `<div>Hi,</div><p>Greetings from  Learnbay,</p> <p>Username:${email}<br/>PassWord:${password}<br/> Login Link:<a href="http://15.207.185.13:3000/">http://15.207.185.13:3000/</a></P>`,
+        };
+
+        transporter.sendMail(mailData, async function (err, info) {
+          if (err) {
+            emailSent = err.message;
+            res.status(200).send({
+              fPdfName: fPdfName,
+              emailSent: emailSent,
+              myPost: myPost,
+              fileUpload: fileUpload,
+            });
+          } else {
+            emailSent = `email sent successfully. ${info.messageId}`;
+
+            res.status(200).json({
+              token: result.insertedId.toString(),
+              email,
+              password,
+              role,
+              team,
+              message: "user created Successful",
+            });
+          }
+        });
 
         // Send all-clear with _id as token
-        res.status(200).json({
-          token: result.insertedId.toString(),
-          email,
-          password,
-          role,
-          team,
-          message: "user created Successful",
-        });
       }
     } catch (error) {
       console.log(error);
--- pages/api/v1/leadSquareData.js
+++
@@ -1,4 +0,0 @@
-export default function handler(req, res) {
-  console.log(req.body);
-  res.status(200).json({ name: req.body });
-}
